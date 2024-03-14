8e783e07ea9f6225a4ffae9bd99079469223c065
Daniel Parks <dp+git@oxidized.org>
2024-03-14 15:08:30 -0700

    First pass at `Roundable` trait
    This was originally part of [`cron-wrapper`], but it made sense to split
    it out.
    
    [`cron-wrapper`]: https://github.com/danielparks/cron-wrapper

--- README.md
+++ README.md
@@ -1,11 +1,34 @@
-# roundable
+# Round numbers and durations to a given factor
 
 [![docs.rs](https://img.shields.io/docsrs/roundable)][docs.rs]
 [![Crates.io](https://img.shields.io/crates/v/roundable)][crates.io]
-![Rust version 1.60+](https://img.shields.io/badge/Rust%20version-1.60%2B-success)
+![Rust version 1.56.1+](https://img.shields.io/badge/Rust%20version-1.56.1%2B-success)
 
-Generated from template https://github.com/danielparks/danielparks-template.rs
-with [cargo-generate](https://github.com/ashleygwilliams/cargo-generate).
+This provides an implementation of rounding for various values, including
+[`std::time::Duration`][`Duration`].
+
+```rust
+use roundable::Roundable;
+
+assert!(310 == 314.round_to(10));
+assert!(300.0 == 314.1.round_to(100.0));
+```
+
+See [Constants][] for a list of time units that make rounding [`Duration`][]
+easier.
+
+```rust
+use roundable::{SECOND, MINUTE, Roundable};
+use std::time::Duration;
+
+assert!(Duration::ZERO == Duration::from_millis(314).round_to(SECOND));
+assert!(MINUTE == Duration::from_millis(59_500).round_to(SECOND));
+```
+
+## Minimum supported Rust version
+
+Currently the minimum supported Rust version (MSRV) is **1.56.1**. Future
+increases in the MSRV will require a major version bump.
 
 ## Development status
 
@@ -28,3 +51,5 @@ additional terms or conditions.
 [docs.rs]: https://docs.rs/roundable/latest/roundable/
 [crates.io]: https://crates.io/crates/roundable
 [issues]: https://github.com/danielparks/roundable/issues
+[`Duration`]: https://doc.rust-lang.org/std/time/struct.Duration.html
+[Constants]: https://docs.rs/roundable/latest/roundable/#Constants
--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,441 @@
-//! # Minimum supported Rust version
+//! # Roundable values
 //!
-//! Currently the minimum supported Rust version (MSRV) is **1.60**. Future
+//! This provides an implementation of rounding for various values, including
+//! [`std::time::Duration`].
+//!
+//! ```rust
+//! use roundable::Roundable;
+//!
+//! assert!(310 == 314.round_to(10));
+//! assert!(300.0 == 314.1.round_to(100.0));
+//! ```
+//!
+//! See [Constants](#constants) for a list of time units that make rounding
+//! [`Duration`] easier.
+//!
+//! ```rust
+//! use roundable::{SECOND, MINUTE, Roundable};
+//! use std::time::Duration;
+//!
+//! assert!(Duration::ZERO == Duration::from_millis(314).round_to(SECOND));
+//! assert!(MINUTE == Duration::from_millis(59_500).round_to(SECOND));
+//! ```
+//!
+//! ## Minimum supported Rust version
+//!
+//! Currently the minimum supported Rust version (MSRV) is **1.56.1**. Future
 //! increases in the MSRV will require a major version bump.
 
-// Most lint configuration is in lints.toml, but that isn’t supported by
-// cargo-geiger, and it only supports deny, not forbid.
+// Lint configuration in Cargo.toml isn’t supported by cargo-geiger.
 #![forbid(unsafe_code)]
 
-/// Encrypt single byte with secure ROT13 function
+use std::time::Duration;
+
+/// A microsecond. Useful for rounding [`Duration`].
 ///
-/// ~~~
-/// use roundable::rot13_u8;
-/// assert_eq!(rot13_u8(b'a'), b'n')
-/// ~~~
-#[must_use]
-pub const fn rot13_u8(c: u8) -> u8 {
-    // The formulas below will never over or underflow.
-    #![allow(clippy::arithmetic_side_effects)]
-    if c.is_ascii_lowercase() {
-        ((c - b'a') + 13) % 26 + b'a'
-    } else if c.is_ascii_uppercase() {
-        ((c - b'A') + 13) % 26 + b'A'
-    } else {
-        c
+/// ```rust
+/// use roundable::{MICROSECOND, Roundable};
+/// use std::time::Duration;
+///
+/// assert!(MICROSECOND == Duration::from_nanos(500).round_to(MICROSECOND));
+/// ```
+pub const MICROSECOND: Duration = Duration::from_micros(1);
+
+/// A millisecond. Useful for rounding [`Duration`].
+///
+/// ```rust
+/// use roundable::{MILLISECOND, Roundable};
+/// use std::time::Duration;
+///
+/// assert!(MILLISECOND == Duration::from_micros(500).round_to(MILLISECOND));
+/// ```
+pub const MILLISECOND: Duration = Duration::from_millis(1);
+
+/// A second. Useful for rounding [`Duration`].
+///
+/// ```rust
+/// use roundable::{SECOND, Roundable};
+/// use std::time::Duration;
+///
+/// assert!(SECOND == Duration::from_millis(500).round_to(SECOND));
+/// ```
+pub const SECOND: Duration = Duration::from_secs(1);
+
+/// A minute. Useful for rounding [`Duration`].
+///
+/// ```rust
+/// use roundable::{MINUTE, Roundable};
+/// use std::time::Duration;
+///
+/// assert!(MINUTE == Duration::from_secs(30).round_to(MINUTE));
+/// ```
+pub const MINUTE: Duration = Duration::from_secs(60);
+
+/// An hour. Useful for rounding [`Duration`].
+///
+/// ```rust
+/// use roundable::{HOUR, Roundable};
+/// use std::time::Duration;
+///
+/// assert!(HOUR == Duration::from_secs(30*60).round_to(HOUR));
+/// ```
+pub const HOUR: Duration = Duration::from_secs(60 * 60);
+
+/// Add methods to round to an arbitrary factor.
+///
+/// For example, you might wish to round an integer to the nearest 10s:
+///
+/// ```rust
+/// use roundable::Roundable;
+///
+/// assert!(310 == 314.round_to(10));
+/// assert!(300 == 314.round_to(100));
+/// ```
+pub trait Roundable: Sized {
+    /// Round to the nearest `factor`.
+    #[must_use]
+    fn round_to(self, factor: Self) -> Self {
+        self.try_round_to(factor).expect("overflow while rounding")
+    }
+
+    /// Round to the nearest `factor`.
+    #[must_use]
+    fn try_round_to(self, factor: Self) -> Option<Self>;
+}
+
+/// Implement rounding for integer types.
+macro_rules! roundable_integer {
+    ($($ty:ident)+) => {$(
+        impl Roundable for $ty {
+            fn try_round_to(self, factor: Self) -> Option<Self> {
+                // FIXME: make into error
+                assert!(factor > 0, "try_round_to() requires positive factor");
+
+                #[allow(clippy::arithmetic_side_effects)]
+                let remainder = self % factor;
+
+                #[allow(unused_comparisons)]
+                if self < 0 {
+                    // Safe: self ≤ remainder ≤ 0, so self - remainder ≥ self
+                    #[allow(clippy::arithmetic_side_effects)]
+                    let base = self - remainder;
+
+                    #[allow(clippy::integer_division)]
+                    #[allow(clippy::arithmetic_side_effects)]
+                    if remainder < factor / 2 + factor % 2 - factor {
+                        // FIXME: document how this can fail and test it
+                        base.checked_sub(factor)
+                    } else {
+                        Some(base)
+                    }
+                } else {
+                    // Safe: 0 ≤ remainder ≤ self
+                    #[allow(clippy::arithmetic_side_effects)]
+                    let base = self - remainder;
+
+                    #[allow(clippy::integer_division)]
+                    #[allow(clippy::arithmetic_side_effects)]
+                    if remainder < factor / 2 + factor % 2 {
+                        Some(base)
+                    } else {
+                        // FIXME: document how this can fail and test it
+                        base.checked_add(factor)
+                    }
+                }
+            }
+        }
+    )+}
+}
+
+roundable_integer!(u8 u16 u32 u64 u128 usize);
+roundable_integer!(i8 i16 i32 i64 i128 isize);
+
+/// Implement rounding for floating point types.
+macro_rules! roundable_float {
+    ($($ty:ident)+) => {$(
+        impl Roundable for $ty {
+            fn try_round_to(self, factor: Self) -> Option<Self> {
+                // FIXME: make into error
+                assert!(factor > 0.0, "try_round_to() requires positive factor");
+
+                #[allow(clippy::arithmetic_side_effects)]
+                let remainder = self % factor;
+                let remainder = if remainder < 0.0 {
+                    factor + remainder
+                } else {
+                    remainder
+                };
+
+                // remainder <= self
+                #[allow(clippy::arithmetic_side_effects)]
+                let base = self - remainder;
+
+                if remainder < factor / 2.0 {
+                    Some(base)
+                } else {
+                    Some(base + factor)
+                }
+            }
+        }
+    )+}
+}
+
+roundable_float!(f32 f64);
+
+impl Roundable for Duration {
+    fn try_round_to(self, factor: Self) -> Option<Self> {
+        // Duration will always fit into u128 as nanoseconds.
+        self.as_nanos()
+            .try_round_to(factor.as_nanos())
+            .map(nanos_to_duration)
     }
 }
 
-/// Encrypt string with secure ROT13 function
+/// Create a new [`Duration`] from a `u128` of nanoseconds.
 ///
-/// ~~~
-/// use roundable::rot13;
-/// assert_eq!(rot13("super secure"), "fhcre frpher")
-/// ~~~
+/// This is essentially just [`Duration::from_nanos()`] but it works on a
+/// `u128`, which can represent any valid `Duration`.
 ///
 /// # Panics
 ///
-/// This could panic if it can’t allocate memory.
+/// `Duration` can only represent 64 bits worth of seconds and less than 32 bits
+/// (1e9) worth of nanoseconds, which works out to being roughly 94 bits. A
+/// `u128` can therefore represent values that are invalid `Duration`s. This
+/// will panic in those cases.
 ///
-/// Strictly speaking, it could panic when converting back to UTF-8, but that
-/// won’t happen because ROT13 only operates on ASCII bytes.
+/// `nanos_to_duration(duration.as_nanos())` should never panic.
+///
+/// ```rust
+/// use roundable::nanos_to_duration;
+/// use std::time::Duration;
+///
+/// assert!(Duration::MAX == nanos_to_duration(Duration::MAX.as_nanos()));
+/// assert!(Duration::ZERO == nanos_to_duration(Duration::ZERO.as_nanos()));
+/// ```
 #[must_use]
-pub fn rot13(source: &str) -> String {
-    let mut buffer: Vec<u8> = Vec::with_capacity(source.len());
-    for c in source.bytes() {
-        buffer.push(rot13_u8(c));
-    }
-    String::from_utf8(buffer).unwrap()
+pub fn nanos_to_duration(total: u128) -> Duration {
+    /// Just to make things clear.
+    const NANOS_PER_SECOND: u128 = 1_000_000_000;
+    #[allow(clippy::integer_division)]
+    Duration::new(
+        (total / NANOS_PER_SECOND).try_into().expect(
+            "nanos_to_duration() overflowed seconds value for Duration",
+        ),
+        (total % NANOS_PER_SECOND).try_into().unwrap(),
+    )
 }
 
 #[cfg(test)]
+#[allow(
+    clippy::cognitive_complexity,
+    clippy::integer_division,
+    clippy::float_cmp
+)]
 mod tests {
     use super::*;
+    use assert2::check;
 
-    macro_rules! test {
-        ($name:ident, $($test:tt)+) => {
-            #[test]
-            fn $name() {
-                ::assert2::assert!($($test)+);
-            }
-        };
+    #[test]
+    fn round_small_unsigned_integer() {
+        check!(10 == 10.round_to(1));
+
+        check!(0 == 0.round_to(2));
+        check!(2 == 1.round_to(2));
+        check!(2 == 2.round_to(2));
+        check!(4 == 3.round_to(2));
+        check!(4 == 4.round_to(2));
+
+        check!(0 == 0.round_to(3));
+        check!(0 == 1.round_to(3));
+        check!(3 == 2.round_to(3));
+        check!(3 == 3.round_to(3));
     }
 
-    test!(byte_tilde, rot13_u8(b'~') == b'~');
-    test!(byte_lower_a, rot13_u8(b'a') == b'a' + 13);
-    test!(byte_upper_a, rot13_u8(b'A') == b'A' + 13);
-    test!(byte_lower_z, rot13_u8(b'z') == b'a' + 12);
-    test!(byte_upper_z, rot13_u8(b'Z') == b'A' + 12);
-    test!(str_abc, rot13(".abc NOP") == ".nop ABC");
+    #[test]
+    fn round_small_signed_integer() {
+        // FIXME: what if factor is negative?
+        check!(10 == 10i8.round_to(1));
+
+        check!(0 == 0i8.round_to(2));
+        check!(2 == 1i8.round_to(2));
+        check!(2 == 2i8.round_to(2));
+        check!(4 == 3i8.round_to(2));
+        check!(4 == 4i8.round_to(2));
+
+        check!(0 == 0i8.round_to(3));
+        check!(0 == 1i8.round_to(3));
+        check!(3 == 2i8.round_to(3));
+        check!(3 == 3i8.round_to(3));
+
+        // Parentheses are to work around a compile failure in check!().
+        check!((-10) == (-10i8).round_to(1));
+
+        check!(0 == (-1i8).round_to(2));
+        check!((-2) == (-2i8).round_to(2));
+        check!((-2) == (-3i8).round_to(2));
+        check!((-4) == (-4i8).round_to(2));
+
+        check!(0 == (-1i8).round_to(3));
+        check!((-3) == (-2i8).round_to(3));
+        check!((-3) == (-3i8).round_to(3));
+    }
+
+    #[test]
+    fn round_max_integer() {
+        check!(0 == 10.round_to(u32::MAX));
+        check!(0 == (u32::MAX / 2).round_to(u32::MAX));
+        check!(u32::MAX == (u32::MAX / 2 + 1).round_to(u32::MAX));
+        check!(u32::MAX == u32::MAX.round_to(u32::MAX));
+
+        check!(0 == 10.round_to(i32::MAX));
+        check!(0 == (i32::MAX / 2).round_to(i32::MAX));
+        check!(i32::MAX == (i32::MAX / 2 + 1).round_to(i32::MAX));
+        check!(i32::MAX == i32::MAX.round_to(i32::MAX));
+    }
+
+    #[test]
+    fn round_min_integer() {
+        check!(-i32::MAX == i32::MIN.round_to(i32::MAX));
+        check!(-i32::MAX == (i32::MIN / 2).round_to(i32::MAX));
+        check!(0 == (i32::MIN / 2 + 1).round_to(i32::MAX));
+    }
+
+    #[test]
+    fn round_small_float() {
+        // FIXME: what if factor is negative?
+        check!(10.0 == 10.0.round_to(1.0));
+
+        check!(0.0 == 0.0.round_to(2.0));
+        check!(2.0 == 1.0.round_to(2.0));
+        check!(2.0 == 2.0.round_to(2.0));
+        check!(4.0 == 3.0.round_to(2.0));
+        check!(4.0 == 4.0.round_to(2.0));
+
+        check!(0.0 == 0.0.round_to(3.0));
+        check!(0.0 == 1.0.round_to(3.0));
+        check!(3.0 == 2.0.round_to(3.0));
+        check!(3.0 == 3.0.round_to(3.0));
+
+        // Parentheses are to work around a compile failure in check!().
+        check!((-10.0) == (-10.0).round_to(1.0));
+
+        check!(0.0 == (-1.0).round_to(2.0));
+        check!((-2.0) == (-2.0).round_to(2.0));
+        check!((-2.0) == (-3.0).round_to(2.0));
+        check!((-4.0) == (-4.0).round_to(2.0));
+
+        check!(0.0 == (-1.0).round_to(3.0));
+        check!((-3.0) == (-2.0).round_to(3.0));
+        check!((-3.0) == (-3.0).round_to(3.0));
+    }
+
+    #[test]
+    fn round_max_float() {
+        check!(0.0 == 10.0.round_to(f32::MAX));
+        check!(0.0 == (f32::MAX * 0.4).round_to(f32::MAX));
+        check!(f32::MAX == (f32::MAX * 0.5).round_to(f32::MAX));
+        check!(f32::MAX == (f32::MAX * 0.6).round_to(f32::MAX));
+    }
+
+    #[test]
+    fn round_min_float() {
+        check!(-f32::MAX == f32::MIN.round_to(f32::MAX));
+        check!(0.0 == (f32::MIN * 0.4).round_to(f32::MAX));
+        check!(0.0 == (f32::MIN * 0.5).round_to(f32::MAX));
+        check!(-f32::MAX == (f32::MIN * 0.6).round_to(f32::MAX));
+    }
+
+    /// Convenient alias for [`Duration::from_millis()`].
+    const fn ms(ms: u64) -> Duration {
+        Duration::from_millis(ms)
+    }
+
+    #[test]
+    fn round_millisecond_to_nearest_millisecond() {
+        check!(ms(10) == ms(10).round_to(MILLISECOND));
+
+        check!(ms(10) == ms(10).round_to(ms(2)));
+        check!(ms(10) == ms(9).round_to(ms(2)));
+
+        check!(ms(9) == ms(9).round_to(ms(3)));
+        check!(ms(9) == ms(10).round_to(ms(3)));
+        check!(ms(12) == ms(11).round_to(ms(3)));
+        check!(ms(12) == ms(12).round_to(ms(3)));
+    }
+
+    #[test]
+    fn round_second_to_nearest_millisecond() {
+        check!(ms(1_010) == ms(1_010).round_to(MILLISECOND));
+
+        check!(ms(1_010) == ms(1_010).round_to(ms(2)));
+        check!(ms(1_010) == ms(1_009).round_to(ms(2)));
+
+        check!(ms(1_008) == ms(1_008).round_to(ms(3)));
+        check!(ms(1_008) == ms(1_009).round_to(ms(3)));
+        check!(ms(1_011) == ms(1_010).round_to(ms(3)));
+        check!(ms(1_011) == ms(1_011).round_to(ms(3)));
+    }
+
+    #[test]
+    fn round_second_to_nearest_second() {
+        check!(ms(0) == ms(499).round_to(SECOND));
+        check!(SECOND == ms(500).round_to(SECOND));
+        check!(SECOND == ms(1_010).round_to(SECOND));
+        check!(SECOND == ms(1_499).round_to(SECOND));
+        check!(ms(2_000) == ms(1_500).round_to(SECOND));
+
+        check!(ms(1_001) == ms(1_000).round_to(ms(1_001)));
+        check!(ms(1_001) == ms(1_001).round_to(ms(1_001)));
+        check!(ms(1_001) == ms(1_002).round_to(ms(1_001)));
+    }
+
+    #[test]
+    fn round_to_giant_factor() {
+        check!(ms(0) == ms(1_000_000).round_to(Duration::MAX));
+        check!(Duration::MAX == Duration::MAX.round_to(Duration::MAX));
+    }
+
+    #[test]
+    #[should_panic(expected = "try_round_to() requires positive factor")]
+    fn round_to_zero_factor() {
+        let _ = ms(10).round_to(ms(0));
+    }
+
+    /// Theoretical maximum Duration as nanoseconds (based on u64 for seconds).
+    const NANOS_MAX: u128 = u64::MAX as u128 * 1_000_000_000 + 999_999_999;
+
+    #[test]
+    #[allow(clippy::arithmetic_side_effects)]
+    fn nanos_to_duration_ok() {
+        check!(Duration::ZERO == nanos_to_duration(0));
+        check!(Duration::new(1, 1) == nanos_to_duration(1_000_000_001));
+
+        // Check Duration::MAX two ways, since according it its docs it can vary
+        // based on platform.
+        check!(Duration::MAX == nanos_to_duration(Duration::MAX.as_nanos()));
+        check!(
+            Duration::new(u64::MAX, 999_999_999)
+                == nanos_to_duration(NANOS_MAX)
+        );
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "nanos_to_duration() overflowed seconds value for Duration: TryFromIntError(())"
+    )]
+    fn nanos_to_duration_overflow() {
+        let _ = nanos_to_duration(Duration::MAX.as_nanos() + 1);
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "nanos_to_duration() overflowed seconds value for Duration: TryFromIntError(())"
+    )]
+    #[allow(clippy::arithmetic_side_effects)]
+    fn nanos_to_duration_overflow_manual() {
+        // One over the maximum duration. Just in case `Duration::MAX` is some
+        // other value, since the docs say it can vary by platform even if it
+        // currently is always `u64::MAX * 1_000_000_000 + 999_999_999`.
+        let _ = nanos_to_duration(NANOS_MAX + 1);
+    }
 }
